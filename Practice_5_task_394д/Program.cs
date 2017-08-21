76d41281652644277cadf6aefdd324f7edc665fc
Maria <Maria@LAPTOP-N156UH30>
2017-08-21 16:43:51 +0500

    Добавьте файлы проекта.

---
+++ Practice_5_task_394д/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_5_task_394д
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
