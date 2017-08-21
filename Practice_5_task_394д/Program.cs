3ea5544dd68f6da08433394788aa562f3e518c89
Maria <Maria@LAPTOP-N156UH30>
2017-08-21 18:19:38 +0500

    программа работает, закончена

--- Practice_5_task_394д/Program.cs
+++ Practice_5_task_394д/Program.cs
@@ -24,11 +24,12 @@ namespace Practice_5_task_394д
                     if ((ok) && (n <= 0)) ok = false;
                 } while (!ok);// конец ввода n с проверкой
 
-                int[,] mas = new int[n, n];
+                int[,] mas = new int[n, n];//исходная матрица
+
                 Console.WriteLine("Введите " + n + " строк по " + n + " целочисленных элементов в каждой, разделенных пробелами:");
                 for (int i=0; i<n; i++)//ввод матрицы с проверкой
                 {
-                    Console.WriteLine("Введите " + i + " строку:");
+                    Console.WriteLine("Введите " + (i+1) + " строку:");
                     do//ввод строки с проверкой
                     {
                         string[] s = Console.ReadLine().Split();//считываем строку в массив из строк
@@ -42,7 +43,7 @@ namespace Practice_5_task_394д
                             for (int j = 0; j < n; j++)
                             {
                                 ok = int.TryParse(s[j], out mas[i, j]);
-                                if (!ok)
+                                if (!ok)//если в строке есть не целые числа или строки
                                 {
                                     Console.WriteLine("Неверный ввод! Введите строку заново:");
                                     break;
@@ -61,7 +62,24 @@ namespace Practice_5_task_394д
                     Console.WriteLine();
                 }
 
-
+                Console.Write("ОТВЕТ: ");
+                bool okey=false;//переменная для проверки, существует ли строка-полиндром в данной матрице. Предполагаем, что ее нет
+                for (int i=0; i<n; i++)//Перебираем строки, ищем полиндромы
+                {
+                    ok = true;//предполагаем, что данная строка - полиндром
+                    for (int j=0; j<n/2; j++)
+                    {
+                        if (mas[i, j] != mas[i, n - j - 1]) ok = false;//если хоть один элемент не совпал, то строка не полиндром
+                    }
+                    if (ok)//если строка полиндром
+                    {
+                        Console.Write((i + 1) + " ");//выводим ее номер
+                        okey = true;//полиндромы есть
+                    }
+                }
+                if (!okey) Console.WriteLine("В данной матрице нет строк-полиндромов");
+                Console.WriteLine();
+                Console.ReadLine();
             } while (true);
         }
     }
